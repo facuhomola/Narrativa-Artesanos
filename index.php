0a605cf06fb183e00542a15aaf958c1cb98ed68d
facuhomola <66138918+facuhomola@users.noreply.github.com>
2022-02-04 09:33:56 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/index.css">
+    <script src="js/bootstrap.min.js"></script>
+    <title>Artesanos.com</title>
+</head>
+<body>
+    
+  <!--Header-->
+  <?php require 'recursos/header.php' ?>
+  <!--Fin Header-->
+
+  <!--Imagenes en Home-->
+  <div class="container-fluid">
+    <div class="row">
+      
+      <div class="col-12 col-md-4 col-sm-12 col-lg-4">
+        <div class="container mb-2">
+          <a href="" class="link-light">
+            <div class="card" style="width: 24rem;">
+              <img src="home-obras/obra1.jpg" class="card-img-top img-obras" alt="obra1">
+              <div class="card-body">
+                <p class="card-text">Pintura en Oleo</p>
+              </div>
+            </div>
+          </a>
+        </div>
+      </div>
+
+      <div class="col-12 col-md-4 col-sm-12 col-lg-4">
+        <div class="container mb-2">
+          <a href="users/u1/u1.php">
+            <div class="card" style="width: 24rem;">
+              <img src="home-obras/obra2.jpg" class="card-img-top img-obras" alt="obra1">
+              <div class="card-body">
+                <p class="card-text">Paisaje colores</p>
+              </div>
+            </div>
+          </a>
+        </div>
+      </div>
+
+      <div class="col-12 col-md-4 col-sm-12 col-lg-4">
+        <div class="container mb-2">
+          <a href="">
+            <div class="card" style="width: 24rem;">
+              <img src="home-obras/obra3.jpg" class="card-img-top img-obras" alt="obra1">
+              <div class="card-body">
+                <p class="card-text">Ciudad Ruinas</p>
+              </div>
+            </div>
+          </a>
+        </div>
+      </div>
+
+    </div>
+
+    <div class="row">
+
+      <div class="col-12 col-md-4 col-sm-12 col-lg-4">
+        <div class="container mb-2">
+          <a href="">
+            <div class="card" style="width: 24rem;">
+              <img src="home-obras/obra4.jpg" class="card-img-top img-obras" alt="obra1">
+              <div class="card-body">
+                <p class="card-text">Kratos -- GOW</p>
+              </div>
+            </div>
+          </a>
+        </div>
+      </div>
+
+      <div class="col-12 col-md-4 col-sm-12 col-lg-4">
+        <div class="container mb-2">
+          <a href="">
+            <div class="card" style="width: 24rem;">
+              <img src="home-obras/obra5.jpg" class="card-img-top img-obras" alt="obra1">
+              <div class="card-body">
+                <p class="card-text">Ellie -- TLOUII</p>
+              </div>
+            </div>
+          </a>
+        </div>
+      </div>
+
+      <div class="col-12 col-md-4 col-sm-12 col-lg-4">
+        <div class="container mb-2">
+          <a href="">
+            <div class="card" style="width: 24rem;">
+              <img src="home-obras/obra6.jpg" class="card-img-top img-obras" alt="obra1">
+              <div class="card-body">
+                <p class="card-text">Dead Space</p>
+              </div>
+            </div>
+          </a>
+        </div>
+      </div>
+
+    </div>
+
+  </div>
+  <!--Fin Imagenes en Home-->
+
+  <footer>
+  </footer>
+
+
+</body>
+</html>
