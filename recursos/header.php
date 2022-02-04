cfc514715dd7720398ad8cb3431e92b06df28628
facuhomola <66138918+facuhomola@users.noreply.github.com>
2022-02-04 09:35:45 -0300

    Add files via upload

---
+++ recursos/header.php
@@ -0,0 +1,46 @@
+<header>
+      <nav class="navbar navbar-expand-lg mb-3">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="/narrativa/index.php">Artesanos</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <form class="d-flex">
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success" type="submit">Search</button>
+          </form>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link" href="#">INICIO</a>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Álbum - Foto
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <li><a class="dropdown-item" href="#">Crear álbum</a></li>
+                  <li><a class="dropdown-item" href="#">Editar álbum</a></li>
+                  <li><a class="dropdown-item" href="#">Eliminar álbum</a></li>
+                  <li><hr class="dropdown-divider"></li>
+                  <li><a class="dropdown-item" href="#">Subir foto</a></li>
+                  <li><a class="dropdown-item" href="#">Editar foto</a></li>
+                  <li><a class="dropdown-item" href="#">Eliminar foto</a></li>
+                </ul>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Mi Perfil
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <li><a class="dropdown-item" href="#">Ver</a></li>
+                  <li><a class="dropdown-item" href="#">Editar</a></li>
+                  <li><a class="dropdown-item" href="#">Denuncias</a></li>
+                  <li><a class="dropdown-item" href="#">Cerrar sesión</a></li>
+                </ul>
+              </li>
+            </ul>
+          </div>
+        </div>
+    </nav>
+  </header>
